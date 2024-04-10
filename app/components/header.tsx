55093d9a48b4ce9d92fffbf42167acf4322b6a91
Marcus Holmgren <marcus.holmgren1@gmail.com>
2024-04-10 17:07:07 +0200

    bugfix: navigation urls

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,8 +3,8 @@ import {Dialog} from "@headlessui/react";
 import {useState} from "react";
 
 const navigation = [
-    {name: 'Direktuppstigning', href: '/'},
-    {name: 'Om', href: 'om'},
+    {name: 'Direktuppstigning', href: '/dykning-med-direktuppstigning/'},
+    {name: 'Om', href: '/dykning-med-direktuppstigning/om'},
 ]
 
 export function Header() {
@@ -14,7 +14,7 @@ export function Header() {
         <header className="absolute inset-x-0 top-0 z-50">
             <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
                 <div className="flex lg:flex-1">
-                    <a href="/" className="-m-1.5 p-1.5">
+                    <a href="/dykning-med-direktuppstigning/" className="-m-1.5 p-1.5">
                         <span className="sr-only">Your Company</span>
                         <img
                             className="h-8 w-auto"
@@ -51,7 +51,7 @@ export function Header() {
                 <Dialog.Panel
                     className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
-                        <a href="/" className="-m-1.5 p-1.5">
+                        <a href="/dykning-med-direktuppstigning/" className="-m-1.5 p-1.5">
                             <span className="sr-only">Your Company</span>
                             <img
                                 className="h-8 w-auto"
